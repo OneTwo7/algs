1ef268a6bf280ae9beb593d5126593e368b0ef6d
OneTwo7 <lahoohal@gmail.com>
2024-03-18 20:10:38 +0400

    heapsort improvement

--- priority-queues/heapsort.ts
+++ priority-queues/heapsort.ts
@@ -1,59 +1,77 @@
-function less<T>(heap: T[], i: number, j: number) {
+type PriorityQueue<T> = {
+  n: number;
+  heap: T[];
+};
+
+function intHalf(k: number) {
+  return Math.floor(k / 2);
+}
+
+function less<T>({ heap }: PriorityQueue<T>, i: number, j: number) {
   return heap[i] < heap[j];
 }
 
-function exch<T>(heap: T[], i: number, j: number) {
+function exch<T>({ heap }: PriorityQueue<T>, i: number, j: number) {
   [heap[i], heap[j]] = [heap[j], heap[i]];
 }
 
-function sink<T>(heap: T[], k: number, n?: number) {
-  if (!n) {
-    n = heap.length - 1;
+function swim<T>(pq: PriorityQueue<T>, k: number) {
+  while (k > 1 && less(pq, intHalf(k), k)) {
+    exch(pq, k, intHalf(k));
+    k = intHalf(k);
   }
+}
 
-  while (k * 2 <= n) {
+function sink<T>(pq: PriorityQueue<T>, k: number) {
+  while (k * 2 <= pq.n) {
     let j = k * 2;
 
-    if (j < n && less(heap, j, j + 1)) {
+    if (j < pq.n && less(pq, j, j + 1)) {
       j++;
     }
 
-    if (!less(heap, k, j)) {
+    if (!less(pq, k, j)) {
       break;
     }
 
-    exch(heap, k, j);
+    exch(pq, k, j);
     k = j;
   }
 }
 
-function delMax<T>(heap: T[]) {
-  let n = heap.length - 1;
-  const max = heap[1];
-  exch(heap, 1, n--);
-  sink(heap, 1, n);
-  heap.length = n + 1;
+function insert<T>(pq: PriorityQueue<T>, item: T) {
+  pq.heap[++pq.n] = item;
+  swim(pq, pq.n);
+}
+
+function delMax<T>(pq: PriorityQueue<T>) {
+  const max = pq.heap[1];
+  exch(pq, 1, pq.n--);
+  sink(pq, 1);
+  delete pq.heap[pq.n + 1];
   return max;
 }
 
-function heapsort<T>(heap: T[]) {
-  const heapClone = structuredClone(heap);
-  const n = heapClone.length - 2;
-  const result: T[] = new Array(n);
+function heapsort<T>(pq: PriorityQueue<T>) {
+  const pqClone = structuredClone(pq);
+  const result: T[] = new Array(pq.n - 1);
 
-  for (let i = n; i >= 0; i--) {
-    result[i] = delMax(heapClone);
+  for (let i = pq.n - 1; i >= 0; i--) {
+    result[i] = delMax(pqClone);
   }
 
   return result;
 }
 
-const a = [null, ...'SORTEXAMPLE'.split('')];
+const priorityQueue: PriorityQueue<string | null> = {
+  n: 0,
+  heap: [null],
+};
 
-for (let i = a.length; i > 0; i--) {
-  sink(a, i);
+for (const char of 'SORTEXAMPLE') {
+  insert(priorityQueue, char);
 }
 
-console.log(`assertion #1: ${a.slice(1).join('') === 'XTSPLRAMOEE'}`);
+console.log(`assertion #1: ${priorityQueue.heap.slice(1).join('') === 'XSTPLRAMOEE'}`);
 
-console.log(`assertion #2: ${heapsort(a).every((item, idx, arr) => item !== null && (idx === 0 || item >= arr[idx - 1]!))}`);
+console.log(`assertion #2: ${heapsort(priorityQueue).every((item, idx, arr) => item !== null && (idx === 0 || item >= arr[idx - 1]!))}`);
